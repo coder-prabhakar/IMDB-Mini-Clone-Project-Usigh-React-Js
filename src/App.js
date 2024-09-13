cd4daf120f184d1256341248449fbe232dde28d0
Prabhakar Kumar <prabhakarkumar16181@gmail.com>
2024-09-13 10:29:53 +0300

    code updated

--- src/App.js
+++ src/App.js
@@ -7,27 +7,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
 
+  // Home Movie List
   const [homeMovieList, setHomeMovieList] = useState(null);
+
+  // Searched keyword
   const [movieList, setMovieList] = useState(null); 
+
+  // Data of a particular movie
   const [movieData, setMovieData] = useState(null);
 
   function getInput(apiSrhType, address, homeMovieShow){
+
     fetch(`https://www.omdbapi.com/?${apiSrhType}=${address}&apikey=612090f1`)
     .then((data)=>{
       return data.json()
     })
     .then((result)=>{ 
+
       if(apiSrhType === "s"){
         if(address.length > 2 && result.Response === "True"){
           setMovieList(result.Search)
-        } else {
-          setMovieList(null)
         }
         (homeMovieShow)?setHomeMovieList(movieList):setHomeMovieList(homeMovieList);
       } else {
-        setMovieData(null)
         setMovieData(result)
       }
+
     });
   }
 
